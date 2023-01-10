db6e94a7e8499799be3575059e4138ddeab68c53
liuxu20 <liuxu20@staff.sina.com.cn>
2023-01-10 16:49:49 +0800

    Kv存储接口定义

---
+++ src/main/java/com/lx/lsmstore/service/KvStore.java
@@ -0,0 +1,30 @@
+package com.lx.lsmstore.service;
+
+import java.io.Closeable;
+
+/**
+ * Kv存储接口定义
+ */
+public interface KvStore extends Closeable {
+
+    /**
+     * 保存数据
+     * @param key
+     * @param value
+     */
+    void set(String key, String value);
+
+    /**
+     * 查询数据
+     * @param key
+     * @return
+     */
+    String get(String key);
+
+    /**
+     * 删除数据
+     * @param key
+     */
+    void rm(String key);
+
+}
